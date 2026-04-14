24e5068d4cf25f3cf9eea8890918049e9610d898
MD. Biplob Molla <ahmedtamim317@gmail.com>
2026-04-14 20:23:47 +0600

    Fontawesome icon cdn Added

--- profile.php
+++ profile.php
@@ -33,7 +33,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="style.css" />
-    <title>My Profile</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Profile</title>
   </head>
   <body>
     <div class="container" id="post-modal">
@@ -92,11 +93,13 @@
       </div>
       <?php if($id != $_SESSION['user_id'] && $id != null) { ?>
       <div class="add-friend">
-        <button>Add Friend</button>
+        <button>Add Friend
+          <i class="fa-solid fa-plus"></i>
+        </button>
       </div>
       <?php } else { ?>
       <div class="edit-profile">
-        <button>Edit Profile</button>
+        <button>Edit Profile <i class="fa-solid fa-pencil"></i></button>
       </div>
       <?php } ?>
 
